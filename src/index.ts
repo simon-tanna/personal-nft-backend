9f878ea7d3d5aed1433353a2893b9a65236dd6d8
simon-tanna <gcab022109@coderacademy.edu.au>
2022-10-01 12:03:36 +1000

    created basic server, utils and log

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+require("dotenv").config;
+import express from "express";
+import config from "config";
+import connectDb from "./utils/connectDb";
+import log from "./utils/logger";
+
+const app = express();
+const port: number = config.get("port");
+
+app.listen(port, () => {
+  log.info(`App running on http://localhost${port}`);
+  connectDb();
+});
---
+++ src/utils/connectDb.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import config from "config";
+import log from "./logger";
+
+const connectDb = async () => {
+  const dbUri = config.get<string>("dbUri");
+
+  try {
+    await mongoose.connect(dbUri);
+    log.info("Connected to Database");
+  } catch (e) {
+    process.exit(1);
+  }
+};
+
+export default connectDb;
