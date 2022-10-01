86e20398a8269049637bec6fc5b7a0a2a8579c29
simon-tanna <gcab022109@coderacademy.edu.au>
2022-10-01 12:45:41 +1000

    created basic route files and user model

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import express from "express";
 import config from "config";
 import connectDb from "./utils/connectDb";
 import log from "./utils/logger";
+import router from "./routes";
 
 const app = express();
+
+app.use(router)
+
 const port: number = config.get("port");
 
 app.listen(port, () => {
